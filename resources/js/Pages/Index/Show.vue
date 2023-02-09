a681469b7642d175487eaa578dd3bf570054939f
Connor Howes <connor@workflowgroup.com>
2023-02-10 00:58:24 +0200

    DesktopUP1

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,9 +1,12 @@
 <template>
-    <div>Show</div>
+     <div>Show</div>
     <Link href="/">Main Page</Link>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/inertia-vue3'
+
+import { Link } from '@inertiajs/vue3'
+import { def } from '@vue/shared';
+
 </script>
 
